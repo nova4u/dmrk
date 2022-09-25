ac332ac2b920b67bf23759d0f2df0b1205ef5fa8
Denis Marushchak <denismar322@gmail.com>
2022-09-25 13:59:46 +1000

    code: add forwardRef to CodeEditor

--- apps/code/src/components/CodeEditor/CodeEditor.tsx
+++ apps/code/src/components/CodeEditor/CodeEditor.tsx
@@ -16,89 +16,90 @@ const EditorTheme: { [key: string]: string } = {
   "--color-prettylights-syntax-entity": colors.slate[200],
   "--color-prettylights-syntax-entity-tag": colors.cyan[500],
   "--color-prettylights-syntax-constant": colors.emerald[400],
-  "--color-canvas-subtle": colors.slate[800],
+  "--color-canvas-subtle": "rgba(23, 23, 23, 0.7)",
   "--color-fg-default": colors.slate[50],
   "--color-prettylights-syntax-string": colors.slate[300],
   "--color-prettylights-syntax-keyword": colors.cyan[500],
 };
 
-const CodeEditor: FC<CodeEditorProps> = ({
-  language,
-  noise,
-  font,
-  fontSize,
-  className,
-}) => {
-  const [code, setCode] = React.useState(
-    `function add(a: number, b: number) {\n  return a + b;\n}`
-  );
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [title, setTitle] = React.useState(`Untitled`);
+const CodeEditor = React.forwardRef<HTMLDivElement, CodeEditorProps>(
+  ({ language, noise, font, fontSize, className }, ref) => {
+    const [code, setCode] = React.useState(
+      `function add(a: number, b: number) {\n  return a + b;\n}`
+    );
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [title, setTitle] = React.useState(`Untitled`);
 
-  const AreaCodeEditor = React.useMemo(
-    () =>
-      dynamic(
-        () =>
-          import("@uiw/react-textarea-code-editor").then((mod) => {
-            setIsLoading(false);
-            return mod.default;
-          }),
-        { ssr: false }
-      ),
-    []
-  );
-
-  if (isLoading)
-    return (
-      <>
-        <span className="loader mt-20 mx-auto"></span>
-        <AreaCodeEditor />
-      </>
+    const AreaCodeEditor = React.useMemo(
+      () =>
+        dynamic(
+          () =>
+            import("@uiw/react-textarea-code-editor").then((mod) => {
+              setIsLoading(false);
+              return mod.default;
+            }),
+          { ssr: false }
+        ),
+      []
     );
 
-  return (
-    <div className="w-full bg-gradient-to-r from-emerald-600 to-green-600 py-14 mt-20 rounded-lg relative">
-      {noise && (
-        <NoiseTexture className="absolute top-0 left-0 -z-0 w-full h-full opacity-40 bg-emerald-600 " />
-      )}
-      <div className={clsx(className)}>
-        <div
-          className={clsx(
-            "h-8 bg-neutral-900/80 rounded-tl-lg rounded-tr-lg grid grid-cols-[100px_1fr_100px]  items-center pl-4 relative"
-          )}
-        >
-          <div className="space-x-2 flex">
-            <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
-            <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
-            <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
+    if (isLoading)
+      return (
+        <>
+          <span className="loader mt-20 mx-auto"></span>
+          <AreaCodeEditor />
+        </>
+      );
+
+    return (
+      <div
+        className="w-full bg-gradient-to-r from-emerald-600 to-green-600 py-14 mt-20 rounded-lg relative"
+        ref={ref}
+      >
+        {noise && (
+          <NoiseTexture className="absolute top-0 left-0 -z-0 w-full h-full opacity-40 bg-emerald-600 " />
+        )}
+        <div className={clsx(className)}>
+          <div
+            className={clsx(
+              "h-8 bg-neutral-900/70 rounded-tl-lg rounded-tr-lg grid grid-cols-[100px_1fr_100px]  items-center pl-4 relative"
+            )}
+          >
+            <div className="space-x-2 flex">
+              <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
+              <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
+              <span className="h-2.5 w-2.5 block rounded-lg bg-zinc-500 hover:bg-zinc-200 transition"></span>
+            </div>
+            <input
+              className="block bg-transparent  focus:outline-none text-center text-xs focus:text-slate-50 text-slate-300 tracking-wider "
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
-          <input
-            className="block bg-transparent  focus:outline-none text-center text-xs focus:text-slate-50 text-slate-300 tracking-wider "
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+          <AreaCodeEditor
+            value={code}
+            language={language}
+            className="rounded-br-lg rounded-bl-lg w-full border border-emerald-50/5 shadow-2xl  tracking-wide bg-neutral-500"
+            placeholder="Please enter JS code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={16}
+            style={
+              {
+                ...EditorTheme,
+                fontFamily: `${
+                  font || "JetBrains Mono"
+                }, ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace`,
+                fontSize,
+              } as React.CSSProperties
+            }
           />
         </div>
-        <AreaCodeEditor
-          value={code}
-          language={language}
-          className="rounded-br-lg rounded-bl-lg w-full border border-emerald-50/5 shadow-2xl  tracking-wide bg-neutral-500"
-          placeholder="Please enter JS code."
-          onChange={(evn) => setCode(evn.target.value)}
-          padding={15}
-          style={
-            {
-              ...EditorTheme,
-              fontFamily: `${
-                font || "JetBrains Mono"
-              }, ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace`,
-              fontSize,
-            } as React.CSSProperties
-          }
-        />
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+CodeEditor.displayName = "CodeEditor";
 
 export default CodeEditor;
 
