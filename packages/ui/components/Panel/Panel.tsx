e12cf64cf56a641fb37dca1d21d4ea1c35c4b777
Denis Marushchak <denismar322@gmail.com>
2022-09-26 21:29:10 +1000

    ui: add forwardRef Panel

--- packages/ui/components/Panel/Panel.tsx
+++ packages/ui/components/Panel/Panel.tsx
@@ -3,23 +3,25 @@ import React, { FC } from "react";
 
 interface PanelProps extends React.HTMLAttributes<HTMLButtonElement> {}
 
-const Panel: FC<React.PropsWithChildren<PanelProps>> = ({
-  className,
-  children,
-}) => {
-  return (
-    <div
-      className={clsx(
-        "  left-1/2 -translate-x-1/2 bottom-10 flex items-center ",
-        className
-      )}
-      style={{
-        width: "calc(100% - 32px)",
-      }}
-    >
-      {children}
-    </div>
-  );
-};
+const Panel = React.forwardRef<HTMLDivElement, PanelProps>(
+  ({ className, children }, ref) => {
+    return (
+      <div
+        className={clsx(
+          "  left-1/2 -translate-x-1/2 bottom-10 flex items-center ",
+          className
+        )}
+        ref={ref}
+        style={{
+          width: "calc(100% - 32px)",
+        }}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Panel.displayName = "Panel";
 
 export default Panel;
