17c2683e1d8883f9b8f2bda412f6d13aa13bf718
Denis Marushchak <denismar322@gmail.com>
2022-10-01 14:58:00 +1000

    change eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,10 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  extends: ['next', 'turbo', 'prettier', 'next/core-web-vitals', 'eslint:recommended'],
+  globals: {
+    React: 'readonly',
   },
-};
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+  },
+}
